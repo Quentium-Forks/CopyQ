0ce858631e45068679e2c8f97e5c7e113a7ff3bd
QuentiumYT <quentin.lienhardt@gmail.com>
2026-02-03 15:21:48 +0100

    Appimage & rpm

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ ARCH_DPKG=$(dpkg --print-architecture)
 export VERSION=$VERSION
 
 # cleanup
-rm -rf build release translations/*.qm # rpm/BUILD rpm/BUILDROOT rpm/*RPMS rpm/SOURCES debug*.list elfbins.list
+rm -rf build release translations/*.qm shared/rpm/BUILD shared/rpm/BUILDROOT shared/rpm/*RPMS shared/rpm/SOURCES debug*.list elfbins.list
 
 # build
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DWITH_NATIVE_NOTIFICATIONS=OFF
@@ -15,7 +15,7 @@ strip -s build/copyq
 
 # assets
 mkdir -p release/$DIR
-cp -r src plugins qxt shared translations CMakeLists.txt debian AUTHORS release/$DIR
+cp -r src plugins qxt shared translations CMakeLists.txt debian AUTHORS CHANGES.md HACKING README.md release/$DIR
 
 # translations
 export QT_SELECT=qt6
@@ -32,18 +32,18 @@ sed -i "s/^Architecture:\s\+.*$/Architecture: $ARCH_DPKG/g" release/$DIR/debian/
 # tarball
 tar -czf release/$DIR.tar.gz -C release $DIR
 
-# # appimagetool
-# wget -qc https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous -O - | grep "appimagetool-.*-$ARCH.AppImage" | head -n 1 | cut -d '"' -f 2) -O appimagetool-$ARCH.AppImage
-# chmod +x appimagetool-$ARCH.AppImage
+# appimagetool
+wget -qc https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous -O - | grep "appimagetool-.*-$ARCH.AppImage" | head -n 1 | cut -d '"' -f 2) -O appimagetool-$ARCH.AppImage
+chmod +x appimagetool-$ARCH.AppImage
 
-# # appimage
-# cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DWITH_NATIVE_NOTIFICATIONS=OFF
-# DESTDIR=../release/$DIR cmake --build build --target install
-# ./appimagetool-$ARCH.AppImage -s deploy release/$DIR/usr/share/applications/copyq.desktop
-# ./appimagetool-$ARCH.AppImage release/$DIR
-# mv copyq-$VERSION-$ARCH.AppImage release
+# appimage
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DWITH_NATIVE_NOTIFICATIONS=OFF
+DESTDIR=../release/$DIR cmake --build build --target install -j $(nproc)
+./appimagetool-$ARCH.AppImage -s deploy release/$DIR/usr/share/applications/com.github.hluk.copyq.desktop
+./appimagetool-$ARCH.AppImage release/$DIR
+mv copyq-$VERSION-$ARCH.AppImage release
 
-# rm appimagetool-$ARCH.AppImage
+rm appimagetool-$ARCH.AppImage
 
 # debian package
 cd release/$DIR
@@ -59,10 +59,10 @@ debuild --preserve-envvar=CMAKE_PREFIX_PATH \
     --no-lintian -us -uc
 cd ../..
 
-# # rpm package
-# mkdir -p rpm/SOURCES
-# cp release/$DIR.tar.gz rpm/SOURCES
-# # Change architecture
-# sed -i "s/^BuildArch:\s\+.*$/BuildArch:      $ARCH/g" rpm/SPECS/copyq.spec
-# rpmbuild -bb --build-in-place --define "_topdir $(pwd)/rpm" rpm/SPECS/copyq.spec
-# mv rpm/RPMS/$ARCH/copyq-$VERSION-1.$ARCH.rpm release/copyq-$VERSION.$ARCH.rpm
+# rpm package
+mkdir -p shared/rpm/SOURCES
+cp release/$DIR.tar.gz shared/rpm/SOURCES
+# Change architecture
+sed -i "s/^BuildArch:\s\+.*$/BuildArch:      $ARCH/g" shared/rpm/SPECS/copyq.spec
+rpmbuild -bb --build-in-place --define "_topdir $(pwd)/shared/rpm" shared/rpm/SPECS/copyq.spec
+mv shared/rpm/RPMS/$ARCH/copyq-$VERSION-1.$ARCH.rpm release/copyq-$VERSION.$ARCH.rpm
---
+++ shared/rpm/SPECS/copyq.spec
@@ -0,0 +1,61 @@
+Name:		copyq
+Version:	13.0.0
+Release:	1%{?dist}
+Summary:	Advanced clipboard manager
+
+License:	GPLv3+
+URL:		https://hluk.github.io/CopyQ/
+Source0:	%{name}-%{version}.tar.gz
+
+# Following line used to track qt5 private api usage
+%{?_qt5:Requires: %{_qt5}%{?_isa} = %{_qt5_version}}
+
+%description
+CopyQ is advanced clipboard manager with searchable and editable history with
+support for image formats, command line control and more.
+
+%prep
+chmod 644 %{SOURCE0}
+%setup -q
+
+
+%build
+cmake -S . -B build \
+    -DCMAKE_INSTALL_PREFIX=%{_prefix} \
+    -DPLUGIN_INSTALL_PREFIX=%{_libdir}/%{name}/plugins \
+    -DDATA_INSTALL_PREFIX=%{_datadir} \
+    -DTRANSLATION_INSTALL_PREFIX=%{_datadir}/%{name}/locale \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_NATIVE_NOTIFICATIONS=OFF
+
+cmake --build build -j $(nproc)
+
+%install
+rm -rf %{buildroot}
+
+DESTDIR=%{buildroot} cmake --install build
+
+%find_lang %{name} --with-qt
+
+%check
+#desktop-file-validate %{buildroot}%{_datadir}/applications/com.github.hluk.%{name}.desktop
+#appstream-util validate-relax --nonet %{buildroot}%{_datadir}/metainfo/com.github.hluk.%{name}.appdata.xml
+
+%files -f %{name}.lang
+%doc AUTHORS CHANGES.md HACKING README.md
+%license LICENSE
+%{_bindir}/%{name}
+%{_libdir}/%{name}/
+%{_datadir}/bash-completion/completions/%{name}
+%{_datadir}/metainfo/com.github.hluk.%{name}.appdata.xml
+%{_datadir}/applications/com.github.hluk.%{name}.desktop
+%dir %{_datadir}/icons/hicolor/*/
+%dir %{_datadir}/icons/hicolor/*/apps/
+%{_datadir}/icons/hicolor/*/apps/%{name}*.png
+%{_datadir}/icons/hicolor/*/apps/%{name}*.svg
+%dir %{_datadir}/%{name}/
+%dir %{_datadir}/%{name}/locale/
+%{_datadir}/%{name}/themes/
+%{_mandir}/man1/%{name}.1.*
+
+%changelog
