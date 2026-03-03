6d0a30ae482552c2f52236d68ea6d5084a557265
QuentiumYT <quentin.lienhardt@gmail.com>
2026-03-03 17:39:38 +0100

    Add release script & automation

---
+++ release.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+VERSION=13.0.0.0
+DIR=copyq-$VERSION
+ARCH=$(uname -m)
+ARCH_DPKG=$(dpkg --print-architecture)
+export VERSION=$VERSION
+
+# cleanup
+rm -rf build release translations/*.qm rpm/BUILD rpm/BUILDROOT rpm/*RPMS rpm/SOURCES debug*.list elfbins.list
+
+# Check if qca dependencies are installed
+if dpkg -s libqca-qt6-dev > /dev/null 2>&1; then
+    EXTRA_FLAGS="-DWITH_QCA_ENCRYPTION=ON"
+else
+    EXTRA_FLAGS="-DWITH_QCA_ENCRYPTION=OFF"
+fi
+# Check if kf6 dependencies are installed
+if dpkg -s libkf6guiaddons-dev > /dev/null 2>&1; then
+    EXTRA_FLAGS="$EXTRA_FLAGS -DWITH_NATIVE_NOTIFICATIONS=ON"
+else
+    EXTRA_FLAGS="$EXTRA_FLAGS -DWITH_NATIVE_NOTIFICATIONS=OFF"
+fi
+
+# build
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr $EXTRA_FLAGS
+cmake --build build -j $(nproc)
+strip -s build/copyq
+
+# assets
+mkdir -p release/$DIR
+cp -r src plugins qxt shared translations CMakeLists.txt debian AUTHORS CHANGES.md HACKING README.md release/$DIR
+
+# translations
+export QT_SELECT=qt6
+# Expand PATH to find lupdate & lrelease
+export PATH="/usr/lib/$QT_SELECT/bin:$PATH"
+lupdate src/ plugins/*/*.{cpp,h,ui} -ts translations/*.ts
+lrelease translations/*.ts
+mkdir -p release/$DIR/copyq/translations
+cp translations/*.qm release/$DIR/copyq/translations
+
+# Change architecture
+sed -i "s/^Architecture:\s\+.*$/Architecture: $ARCH_DPKG/g" release/$DIR/debian/control
+
+# tarball
+tar -czf release/$DIR.tar.gz -C release $DIR
+
+# appimagetool
+wget -qc https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous -O - | grep "appimagetool-.*-$ARCH.AppImage" | head -n 1 | cut -d '"' -f 2) -O appimagetool-$ARCH.AppImage
+chmod +x appimagetool-$ARCH.AppImage
+
+# appimage
+DESTDIR=../release/$DIR cmake --build build --target install -j $(nproc)
+./appimagetool-$ARCH.AppImage -s deploy release/$DIR/usr/share/applications/com.github.hluk.copyq.desktop
+./appimagetool-$ARCH.AppImage release/$DIR
+mv CopyQ-$VERSION-$ARCH.AppImage release
+
+rm appimagetool-$ARCH.AppImage
+
+# debian package
+cd release/$DIR
+# Export CMake prefix path for debuild using aqtinstall
+if [ -z "$QT_PLUGIN_PATH" ]; then
+    QT_ROOT=$(dirname "$QT_PLUGIN_PATH")
+    export CMAKE_PREFIX_PATH="$QT_ROOT/lib/cmake"
+fi
+dh_make --createorig --indep --yes
+debuild --preserve-envvar=CMAKE_PREFIX_PATH \
+    --preserve-envvar=QT_PLUGIN_PATH \
+    --preserve-envvar=LD_LIBRARY_PATH \
+    --no-lintian -us -uc
+cd ../..
+
+# rpm package
+mkdir -p rpm/SOURCES
+cp release/$DIR.tar.gz rpm/SOURCES
+# Change architecture
+sed -i "s/^BuildArch:\s\+.*$/BuildArch:      $ARCH/g" rpm/SPECS/copyq.spec
+rpmbuild -bb --build-in-place --define "_topdir $(pwd)/rpm" rpm/SPECS/copyq.spec
+mv rpm/RPMS/$ARCH/copyq-$VERSION-1.$ARCH.rpm release/copyq-$VERSION.$ARCH.rpm
---
+++ rpm/SPECS/copyq.spec
@@ -0,0 +1,73 @@
+Name:       copyq
+Version:    13.0.0.0
+Release:    1%{?dist}
+Summary:    Advanced clipboard manager
+
+License:    GPLv3+
+URL:        https://hluk.github.io/CopyQ/
+Source0:    %{name}-%{version}.tar.gz
+
+%description
+CopyQ is advanced clipboard manager with searchable and editable history with
+support for image formats, command line control and more.
+
+%prep
+%setup -q
+
+%build
+# Check if qca dependencies are installed
+if rpm -q qca-qt6-devel; then
+    EXTRA_FLAGS="-DWITH_QCA_ENCRYPTION=ON"
+else
+    EXTRA_FLAGS="-DWITH_QCA_ENCRYPTION=OFF"
+fi
+# Check if kf6 dependencies are installed
+if rpm -q kf6-kguiaddons-devel; then
+    EXTRA_FLAGS="$EXTRA_FLAGS -DWITH_NATIVE_NOTIFICATIONS=ON"
+else
+    EXTRA_FLAGS="$EXTRA_FLAGS -DWITH_NATIVE_NOTIFICATIONS=OFF"
+fi
+
+cmake -S . -B build \
+    -DCMAKE_INSTALL_PREFIX=%{_prefix} \
+    -DPLUGIN_INSTALL_PREFIX=%{_libdir}/%{name}/plugins \
+    -DTRANSLATION_INSTALL_PREFIX=%{_datadir}/%{name}/locale \
+    -DCMAKE_BUILD_TYPE=Release \
+    $EXTRA_FLAGS
+
+cmake --build build -j $(nproc)
+
+%install
+rm -rf %{buildroot}
+
+DESTDIR=%{buildroot} cmake --install build
+
+%find_lang %{name} --with-qt
+
+%check
+if command -v desktop-file-validate >/dev/null 2>&1; then
+    desktop-file-validate %{buildroot}%{_datadir}/applications/com.github.hluk.%{name}.desktop
+fi
+if command -v appstream-util >/dev/null 2>&1; then
+    appstream-util validate-relax --nonet %{buildroot}%{_datadir}/metainfo/com.github.hluk.%{name}.appdata.xml
+fi
+
+%files -f %{name}.lang
+%doc AUTHORS CHANGES.md HACKING README.md
+%license LICENSE
+%{_bindir}/%{name}
+%{_libdir}/%{name}/
+%{_datadir}/bash-completion/completions/%{name}
+%{_datadir}/metainfo/com.github.hluk.%{name}.appdata.xml
+%{_datadir}/applications/com.github.hluk.%{name}.desktop
+%dir %{_datadir}/icons/hicolor/*/
+%dir %{_datadir}/icons/hicolor/*/apps/
+%{_datadir}/icons/hicolor/*/apps/%{name}*.png
+%{_datadir}/icons/hicolor/*/apps/%{name}*.svg
+%dir %{_datadir}/%{name}/
+%dir %{_datadir}/%{name}/locale/
+%{_datadir}/%{name}/themes/
+%{_datadir}/gnome-shell/extensions/%{name}-clipboard@hluk.github.com/
+%{_mandir}/man1/%{name}.1.*
+
+%changelog
