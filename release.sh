59eb6cc922055904d2f0f19de1ec4b65aad825f1
QuentiumYT <quentin.lienhardt@gmail.com>
2026-03-18 14:12:43 +0100

    new lines debian rules typo

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=13.0.0.3
+VERSION=13.0.0.4
 DIR=copyq-$VERSION
 ARCH=$(uname -m)
 ARCH_DPKG=$(dpkg --print-architecture)
@@ -28,22 +28,22 @@ else
 fi
 
 # build
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr $EXTRA_FLAGS
-cmake --build build -j $(nproc)
-strip -s build/copyq
+# cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr $EXTRA_FLAGS
+# cmake --build build -j $(nproc)
+# strip -s build/copyq
 
 # assets
 mkdir -p release/$DIR
 cp -r src plugins qxt shared translations CMakeLists.txt debian AUTHORS CHANGES.md HACKING README.md release/$DIR
 
-# translations
-export QT_SELECT=qt6
-# Expand PATH to find lupdate & lrelease
-export PATH="/usr/lib/$QT_SELECT/bin:$PATH"
-lupdate src/ plugins/*/*.{cpp,h,ui} -ts translations/*.ts
-lrelease translations/*.ts
-mkdir -p release/$DIR/copyq/translations
-cp translations/*.qm release/$DIR/copyq/translations
+# # translations
+# export QT_SELECT=qt6
+# # Expand PATH to find lupdate & lrelease
+# export PATH="/usr/lib/$QT_SELECT/bin:$PATH"
+# lupdate src/ plugins/*/*.{cpp,h,ui} -ts translations/*.ts
+# lrelease translations/*.ts
+# mkdir -p release/$DIR/copyq/translations
+# cp translations/*.qm release/$DIR/copyq/translations
 
 # Change architecture
 sed -i "s/^Architecture:\s\+.*$/Architecture: $ARCH_DPKG/g" release/$DIR/debian/control
@@ -51,17 +51,17 @@ sed -i "s/^Architecture:\s\+.*$/Architecture: $ARCH_DPKG/g" release/$DIR/debian/
 # tarball
 tar -czf release/$DIR.tar.gz -C release $DIR
 
-# appimagetool
-wget -qc https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous -O - | grep "appimagetool-.*-$ARCH.AppImage" | head -n 1 | cut -d '"' -f 2) -O appimagetool-$ARCH.AppImage
-chmod +x appimagetool-$ARCH.AppImage
+# # appimagetool
+# wget -qc https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous -O - | grep "appimagetool-.*-$ARCH.AppImage" | head -n 1 | cut -d '"' -f 2) -O appimagetool-$ARCH.AppImage
+# chmod +x appimagetool-$ARCH.AppImage
 
-# appimage
-DESTDIR=../release/$DIR cmake --build build --target install -j $(nproc)
-./appimagetool-$ARCH.AppImage -s deploy release/$DIR/usr/share/applications/com.github.hluk.copyq.desktop
-./appimagetool-$ARCH.AppImage release/$DIR
-mv CopyQ-$VERSION-$ARCH.AppImage release
+# # appimage
+# DESTDIR=../release/$DIR cmake --build build --target install -j $(nproc)
+# ./appimagetool-$ARCH.AppImage -s deploy release/$DIR/usr/share/applications/com.github.hluk.copyq.desktop
+# ./appimagetool-$ARCH.AppImage release/$DIR
+# mv CopyQ-$VERSION-$ARCH.AppImage release
 
-rm appimagetool-$ARCH.AppImage
+# rm appimagetool-$ARCH.AppImage
 
 # debian package
 cd release/$DIR
@@ -77,10 +77,10 @@ debuild --preserve-envvar=CMAKE_PREFIX_PATH \
     --no-lintian -us -uc
 cd ../..
 
-# rpm package
-mkdir -p rpm/SOURCES
-cp release/$DIR.tar.gz rpm/SOURCES
-# Change architecture
-sed -i "s/^BuildArch:\s\+.*$/BuildArch:      $ARCH/g" rpm/SPECS/copyq.spec
-rpmbuild -bb --build-in-place --define "_topdir $(pwd)/rpm" rpm/SPECS/copyq.spec
-mv rpm/RPMS/$ARCH/copyq-$VERSION-1.$ARCH.rpm release/copyq-$VERSION.$ARCH.rpm
+# # rpm package
+# mkdir -p rpm/SOURCES
+# cp release/$DIR.tar.gz rpm/SOURCES
+# # Change architecture
+# sed -i "s/^BuildArch:\s\+.*$/BuildArch:      $ARCH/g" rpm/SPECS/copyq.spec
+# rpmbuild -bb --build-in-place --define "_topdir $(pwd)/rpm" rpm/SPECS/copyq.spec
+# mv rpm/RPMS/$ARCH/copyq-$VERSION-1.$ARCH.rpm release/copyq-$VERSION.$ARCH.rpm
